fc46042dad130d7c5c5932b435433daad7d17aa2
Lev Kaplunov <limon4ik59rus@mail.ru>
2022-01-27 15:30:12 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <MyComponent></MyComponent>
+  </div>
+</template>
+
+<script>
+
+import MyComponent from "./components/MyComponent";
+export default {
+  name: 'App',
+	components: {MyComponent},
+}
+</script>
